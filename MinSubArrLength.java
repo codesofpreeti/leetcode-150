08bd21f90096b1c67aab8839f7b74a2fd3e49bee
codesofpreeti <preetiipriyaa@gmail.com>
2024-04-03 05:34:39 +0530

    minSubarray

---
+++ MinSubArrLength.java
@@ -0,0 +1,22 @@
+/**
+ * MinSubArrLength
+ */
+public class MinSubArrLength {
+
+    public int MinSub(int nums[],int target){
+        int res=Integer.MAX_VALUE;
+        int left=0;
+        int right=0;
+        int sum=0;
+        while(right<nums.length){
+            sum+=nums[right];
+            while (sum>=target) {
+                res=Math.min(res,right-left+1);
+                sum-=nums[left];
+                left--;
+            }
+            right++;
+        }
+        return res;
+    }
+}
