4dc768b36e015f58995e27ad715d14d5e939f2a0
codesofpreeti <preetiipriyaa@gmail.com>
2024-07-06 00:15:28 +0530

    july 05

---
+++ Backtracking/WordSearch.java
@@ -0,0 +1,46 @@
+package Backtracking;
+
+public class WordSearch {
+    int dirs[][]={{1,0},{-1,0},{0,1},{0,-1}};
+    int m,n;
+
+    public boolean exist(char[][] board, String word) {
+        m=board.length;
+        n=board[0].length;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word.charAt(0) && find(board,i, j, 0, word)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean find(char[][] board,int i, int j, int idx, String word){
+        if(idx==word.length()){
+            return true;
+        }
+        if(i<0 || j<0 || i>=m ||j>=n ||board[i][j]=='$'){
+            return false;
+        }
+        if(board[i][j]!=word.charAt(idx)){
+            return false;
+        }
+         
+        char temp=board[i][j];
+        board[i][j]='$';
+
+        for(int nums[]:dirs){
+            int new_i=i+nums[0];
+            int new_j=j+nums[1];
+
+            if(find(board,new_i,new_j,idx+1,word)){
+                return true;
+            }
+        }
+        board[i][j]=temp;
+        return false;
+    }
+    
+}
