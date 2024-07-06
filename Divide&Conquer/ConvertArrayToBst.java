fa1e801dae73a3ebfcb5f4e8cd2225347b5642d4
codesofpreeti <preetiipriyaa@gmail.com>
2024-07-06 20:29:35 +0530

    july 06

---
+++ Divide&Conquer/ConvertArrayToBst.java
@@ -0,0 +1,19 @@
+package Divide&Conquer;
+
+public class ConvertArrayToBst {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return BST(nums,0,nums.length-1);
+    }
+    public TreeNode BST(int[] nums, int start, int end){
+        if(start>end){
+            return null;
+        }
+        int mid=start+(end-start)/2;
+        
+        TreeNode node=new TreeNode(nums[mid]);
+        node.left=BST(nums,start,mid-1);
+        node.right=BST(nums,mid+1,end);
+
+        return node;
+    }
+}
